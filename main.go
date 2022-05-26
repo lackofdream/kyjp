89c7184d71cf5bf447b48e1d62cb6786d4a69efd
LackOfDream <galaxy0419@gmail.com>
2022-05-26 15:35:56 +0900

    add nex

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"gopkg.in/yaml.v3"
 	"log"
 	"net/http"
 	"strings"
 )
+
+func getYaml(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("request failed: %d", resp.StatusCode))
+	}
+	var config interface{}
+	err = yaml.NewDecoder(resp.Body).Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return config, nil
+}
 
 func main() {
 	http.HandleFunc("/ky", func(w http.ResponseWriter, r *http.Request) {
@@ -15,30 +34,50 @@ func main() {
 			}
 		}()
 		url := r.URL.Query().Get("url")
-		resp, err := http.Get(url)
+		config, err := getYaml(url)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = w.Write([]byte(err.Error()))
 			return
 		}
-		if resp.StatusCode != http.StatusOK {
-			w.WriteHeader(resp.StatusCode)
-			return
-		}
-		var config interface{}
-		err = yaml.NewDecoder(resp.Body).Decode(&config)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			_, _ = w.Write([]byte(err.Error()))
-			return
-		}
-		defer resp.Body.Close()
 		proxies := config.(map[string]interface{})["proxies"].([]interface{})
 		var filtered []interface{}
 		for _, p := range proxies {
 			pp := p.(map[string]interface{})
 			if strings.Contains(pp["name"].(string), "日本") &&
-				!strings.Contains(pp["name"].(string), "仅海外用户"){
+				!strings.Contains(pp["name"].(string), "仅海外用户") {
+				filtered = append(filtered, p)
+			}
+		}
+		result, err := yaml.Marshal(map[string][]interface{}{
+			"proxies": filtered,
+		})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write(result)
+	})
+
+	http.HandleFunc("/nex", func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
+		url := r.URL.Query().Get("url")
+		config, err := getYaml(url)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+		proxies := config.(map[string]interface{})["proxies"].([]interface{})
+		var filtered []interface{}
+		for _, p := range proxies {
+			pp := p.(map[string]interface{})
+			if strings.Contains(pp["name"].(string), "Japan") {
 				filtered = append(filtered, p)
 			}
 		}
